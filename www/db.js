653d181d503413281dd42e841285ddff02f2b918
memoona-tahira <memoona.tahira@gmail.com>
2020-02-27 21:36:25 +0100

    chnages for task 1

--- www/db.js
+++ www/db.js
@@ -22,7 +22,7 @@ var p2 = new Patiant("b", 1, 23)
 var p3 = new Patiant("c", 1, 23)
 var p4 = new Patiant("d", 1, 23)
 
-var d = new Docto("doctor", 45, [p1, p2, p3, p4])
+var d = new Docto("Shaista", 45, [p1, p2, p3, p4])
 
 console.log(d)
 
