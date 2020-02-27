381c89a2d6a1b01e8f63231100da9f8473b2d848
memoona-tahira <memoona.tahira@gmail.com>
2020-02-27 21:18:10 +0100

    my changes to db.js

--- www/db.js
+++ www/db.js
@@ -10,16 +10,17 @@ class Docto {
 }
 
 class Patiant {
-  constructor(name, age) {
+  constructor(name, age, phone) {
     this.name = name;
     this.age = age;
+    this.phone = phone
   }
 }
 
-var p1 = new Patiant("a", 1)
-var p2 = new Patiant("b", 1)
-var p3 = new Patiant("c", 1)
-var p4 = new Patiant("d", 1)
+var p1 = new Patiant("a", 1, 23)
+var p2 = new Patiant("b", 1, 23)
+var p3 = new Patiant("c", 1, 23)
+var p4 = new Patiant("d", 1, 23)
 
 var d = new Docto("doctor", 45, [p1, p2, p3, p4])
 
